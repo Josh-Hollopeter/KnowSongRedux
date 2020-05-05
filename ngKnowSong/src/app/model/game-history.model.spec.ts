0aab9fc86da91c1d4f455d57adb53cb4e716137f
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-05-05 16:15:03 -0400

    added basic user and game history to angular

---
+++ ngKnowSong/src/app/model/game-history.model.spec.ts
@@ -0,0 +1,7 @@
+import { GameHistory } from './game-history.model';
+
+describe('GameHistory', () => {
+  it('should create an instance', () => {
+    expect(new GameHistory()).toBeTruthy();
+  });
+});
