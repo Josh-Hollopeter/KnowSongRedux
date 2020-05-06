d356218144e53e794cc38376e1735cee5afe7760
NealSavant <33883884+NealSavant@users.noreply.github.com>
2020-05-06 16:56:15 -0600

    Model setup

--- ngKnowSong/src/app/model/user.model.ts
+++ ngKnowSong/src/app/model/user.model.ts
@@ -1,14 +1,29 @@
+import { GameHistory } from './game-history.model';
+
 export class User {
-    //this is where user info goes :)
-    //comment
-    //kiling it with the commits
-    //bring on the green boys
-    //this is where user info goes :)
-    //comment
-    //kiling it with the commits
-    //bring on the green boys
-    //this is where user info goes :)
-    //comment
-    //kiling it with the commits
-    //bring on the green boys
+    public userId: number;
+    public username: string;
+    public password: string;
+    public authToken :string;
+    public rankImg : string;
+    public imgSource : string;
+    public enabled : boolean;
+    public role : string;
+    public gameHistory: GameHistory;
+    public gameHistories: GameHistory[];
+  
+  
+  
+  
+    constructor(userId?: number, username?: string, password?: string,authToken?:string, rankImg?: string, imgSource?: string, enabled?: boolean, role?: string,gameHistory?:GameHistory,gameHistories?:[]){
+      this.userId = userId;
+      this.username = username;
+      this.password = password;
+      this.authToken= authToken;
+      this.rankImg = rankImg;
+      this.imgSource = imgSource;
+      this.enabled = enabled;
+      this.role = role;
+      this.gameHistories = gameHistories;
+    }
 }
