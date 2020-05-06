55443c17f19607f1e46077847a2937eb16c7f401
NealSavant <33883884+NealSavant@users.noreply.github.com>
2020-05-06 17:30:50 -0600

    setup modules and router

--- ngKnowSong/src/app/app-routing.module.ts
+++ ngKnowSong/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './registration/register/register.component';
+import { LoginComponent } from './registration/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: RegisterComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
