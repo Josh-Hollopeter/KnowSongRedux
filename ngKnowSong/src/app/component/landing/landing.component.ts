8a5adbb4a773fb6788091f5d581ed5267218dbb0
NealSavant <33883884+NealSavant@users.noreply.github.com>
2020-05-07 19:31:57 -0600

    client on angular is almost working, redirect-uri is showing up for spring not 4200

--- ngKnowSong/src/app/component/landing/landing.component.ts
+++ ngKnowSong/src/app/component/landing/landing.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user.model';
 
+// landing page where users can login through spotify
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-// landing page where users can login through spotify
 
-  constructor() { }
+
+  user: User = new User();
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  requestAuth(){
+    window.open('http://localhost:8085/oauth2/authorization/spotify');
+  }
   login(){
     console.log("logging in and stuff");
+    this.authService.login().subscribe(
+      success=> {
+        console.log(success);
+        this.user.username = success["spotifyId"];
+        // this.user.imgSource = success["spotifyImg"];
+        // this.user.role = success["role"];
+        // this.user.accessToken = success["accessToken"];//i think we put this on header
+        // this.user.enabled = success["enabled"];
+      },
+      failure => {
+        console.log("user login failed in landing component");
+        console.log(failure);
+      }
+    )
     // window.location.href = 'http://localhost:8085/'
   }
 }
