0d78c0ca1a23a59a8b5879c964a6c6d7f9aba864
NealSavant <nealrsavant@gmail.com>
2020-05-09 14:02:43 -0600

    fixed cors, allowing all requests to spring servlet

--- KnowSongRest/src/main/java/com/skilldistillery/knowsong/security/SecurityConfig.java
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.skilldistillery.knowsong.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
@@ -11,9 +10,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -40,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	// @formatter:off
         http
             .authorizeRequests(a -> a
-                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
             )
             .exceptionHandling(e -> e
