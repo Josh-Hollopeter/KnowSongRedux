aa1b595421db313bb7eb74f6b00cb3b65f61741f
NealSavant <nealrsavant@gmail.com>
2020-05-09 16:24:04 -0600

    Wrote a custom userInfoEndpoint for grabbing user details upon login. now to import the user attributes from spotify auth resource server to our database. lets fucking gooo https://docs.spring.io/spring-security/site/docs/current/reference/html5/#oauth2login-advanced for the documentation i just read for 3 hours lmaooo

---
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/services/CustomOAuth2UserService.java
@@ -0,0 +1,29 @@
+package com.skilldistillery.knowsong.services;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+import com.skilldistillery.knowsong.repositories.UserRepository;
+
+@Component
+public class CustomOAuth2UserService extends DefaultOAuth2UserService{
+
+	@Autowired
+	UserRepository userRepo;
+	
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
+		//create new user object and get the attributes
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+		Map<String, Object> attributes = oAuth2User.getAttributes();
+		System.out.println(attributes);
+		
+		return oAuth2User;
+	}
+}
