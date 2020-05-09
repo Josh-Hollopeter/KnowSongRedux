c2b010ddc69822b9d0e49c1757c237df548a6374
NealSavant <nealrsavant@gmail.com>
2020-05-09 13:21:11 -0600

    Clean up authservice

--- ngKnowSong/src/app/service/auth.service.ts
+++ ngKnowSong/src/app/service/auth.service.ts
@@ -28,7 +28,8 @@ export class AuthService {
         'Content-Type': 'text/plain; charset=utf-8'
       })
     };
-console.log(credentials);
+   
+     console.log(credentials);
 
      return this.http.get(this.baseUrl + 'user', httpOptions).pipe(       
       tap((res: any) => {
@@ -42,67 +43,6 @@ console.log(credentials);
   );
 
   }
-
-  /********************************************************************************
-  * Request Authorization:
-  * - Get redirect URI from Spring Servlet Controller 
-  ********************************************************************************/
- requestAuthorization() {
-  var credentials = this.getCredentials;
-  let state = this.generateStateString(16);
-
-  const headers = new HttpHeaders();
-  headers.set('Authorization', `Basic ${credentials}`);
-  headers.set('Content-Type', 'text/plain; charset=utf-8');
-
-  return this.http
-  .post(this.baseUrl + 'getAuthorized', state, {headers, responseType:'text'})
-  .pipe(
-    catchError((err: any) => {
-      return throwError('AuthService.requestAuthorization(): Error getting redirect uri.');
-    })
-  );
-}
-
-/********************************************************************************
-* GenerateStateString:
-* - What: generate random string of characters
-* - Why: prevents against cross-site request forgery
-********************************************************************************/
-generateStateString(length: number): string{
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for ( let x = 0; x < length; x++ ){
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-/********************************************************************************
-* Authorize User:
-* - 1st) verify that state string is the identical 
-* - 2nd) send OAuth2 API Server code to our internal endpoint to retrieve OAuth2 Access Token
-********************************************************************************/
-authorizeUser(code: string, state: string){
-  
-  let packet = code + "," + state;
-  
-  var credentials = this.getCredentials; 
-  const httpOptions = {
-    headers: new HttpHeaders({
-      'Authorization': `Basic ${credentials}`,
-      'X-Requested-With': 'XMLHttpRequest',
-      'Content-Type': 'text/plain; charset=utf-8'
-    })
-  };
-
-  //send packet to Spring Controller
-  return this.http.post(this.baseUrl + 'authorizeUser', packet, httpOptions).pipe(
-    catchError((err: any) => {
-      return throwError('AuthService.authorizeUser(): Error getting access token :(');
-    })
-  );
-}
 
 getCredentials() {
   return localStorage.getItem('credentials');
