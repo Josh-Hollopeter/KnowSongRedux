b7ea594e7485aa2c36ef74063273a1ce394dc96c
NealSavant <33883884+NealSavant@users.noreply.github.com>
2020-05-08 20:04:51 -0600

    server completes oauth handoff placing cookie in browser and on success redirects user to angular client site. Now i must accomplish angular requests using the authentication for information about the user

--- KnowSongRest/src/main/java/com/skilldistillery/knowsong/controllers/UserController.java
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/controllers/UserController.java
@@ -3,20 +3,43 @@ package com.skilldistillery.knowsong.controllers;
 import java.util.Collections;
 import java.util.Map;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins= "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
+//	@GetMapping("/user")
+//    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+//        return Collections.singletonMap("username", principal.getAttribute("id"));
+//    }
+
 	@GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("username", principal.getAttribute("id"));
-    }
-	
-	
+	public OAuth2User user(@AuthenticationPrincipal OAuth2User oauth2User) {
+		return oauth2User;
+	}
+
+//	@GetMapping("/")
+//	public OAuth2AccessToken accessToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+//		return authorizedClient.getAccessToken();
+//	}
+
+	@GetMapping("/")
+	public OAuth2AuthorizedClient accessToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+		return authorizedClient;
+	}
+//	public OAuth2User getCurrentUser() {
+//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//		return ((OAuth2AuthenticationToken)auth).getPrincipal();
+//	}
 }
