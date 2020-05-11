caefd2fdcbe32b54a380c34c8a03ecf047025277
NealSavant <nealrsavant@gmail.com>
2020-05-11 16:47:45 -0600

    figuring out what the error looks like when requesting a GET with an expired access token... guess i wait an hour to see what that looks like?lmaoo

--- KnowSongRest/src/main/java/com/skilldistillery/knowsong/security/SecurityConfig.java
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/security/SecurityConfig.java
@@ -9,11 +9,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -37,8 +44,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 
             )
-//            .sessionManagement()
-//            	.disable()
             .exceptionHandling(e -> e
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             )
@@ -47,12 +52,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                     )
             .oauth2Login()
+//            	.tokenEndpoint()
+//            		.accessTokenResponseClient(accessTokenResponseClient())
+//            	.and()
             	.userInfoEndpoint()
             			.userService(userService) // upon login, user information is opened in this service. conditionals applied in here
             	.and()
             	.defaultSuccessUrl("http://localhost:4200/home")
             	.failureUrl("http://localhost:4200");
+   
     }
+    
+//    @Bean
+//    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient(){
+//    	DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient = 
+//    	          new DefaultAuthorizationCodeTokenResponseClient(); 
+//    	        accessTokenResponseClient.setRequestEntityConverter(new CustomRequestEntityConverter()); 
+//    	 
+//    	        OAuth2AccessTokenResponseHttpMessageConverter tokenResponseHttpMessageConverter = 
+//    	          new OAuth2AccessTokenResponseHttpMessageConverter(); 
+//    	        tokenResponseHttpMessageConverter.setTokenResponseConverter(new CustomTokenResponseConverter()); 
+//    	        RestTemplate restTemplate = new RestTemplate(Arrays.asList(
+//    	          new FormHttpMessageConverter(), tokenResponseHttpMessageConverter)); 
+//    	        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler()); 
+//    	         
+//    	        accessTokenResponseClient.setRestOperations(restTemplate); 
+//    	
+//    	
+//    	return accessTokenResponseClient;
+//    }
     
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
