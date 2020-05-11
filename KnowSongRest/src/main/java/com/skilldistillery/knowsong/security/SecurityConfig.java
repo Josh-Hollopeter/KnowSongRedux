f6dca9f414ec1b400cd99da8b0b9b7b815903a35
NealSavant <nealrsavant@gmail.com>
2020-05-11 13:58:47 -0600

    Front end applies backend xhr request properly with HTTPOnly flagged cookies included. CORS errors from backend solved by creating custom corsConfigurationSource in security config. FINALLY:our front end client is authenticated with spring and can view access token

--- KnowSongRest/src/main/java/com/skilldistillery/knowsong/security/SecurityConfig.java
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.skilldistillery.knowsong.security;
 
+import java.util.Arrays;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
@@ -11,6 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.skilldistillery.knowsong.repositories.UserRepository;
 import com.skilldistillery.knowsong.services.CustomOAuth2UserService;
@@ -29,10 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests(a -> a
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
+                
             )
+//            .sessionManagement()
+//            	.disable()
             .exceptionHandling(e -> e
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             )
+            .cors().and()
             .csrf(c -> c
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                     )
@@ -42,6 +52,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             	.and()
             	.defaultSuccessUrl("http://localhost:4200/home")
             	.failureUrl("http://localhost:4200");
+    }
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+    	CorsConfiguration configuration = new CorsConfiguration();
+    	configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+    	configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT"));
+    	configuration.setAllowedHeaders(Arrays.asList("Content-Type", "credentials"));
+    	configuration.setAllowCredentials(true);
+    	
+    	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    	source.registerCorsConfiguration("/**", configuration);
+    	
+    	return source;
     }
 
    
