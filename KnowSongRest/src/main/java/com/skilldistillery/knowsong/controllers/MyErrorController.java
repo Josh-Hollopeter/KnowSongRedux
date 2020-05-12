e14b1e51d1f233f7b4b16bf17c4e39b56eaeca60
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-05-12 11:07:22 -0400

    working deployment

--- KnowSongRest/src/main/java/com/skilldistillery/knowsong/controllers/MyErrorController.java
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/controllers/MyErrorController.java
@@ -9,7 +9,7 @@ public class MyErrorController implements ErrorController {
 
     @RequestMapping("/error")
     public String handleError() {
-        return "redirect:#home";
+        return "#home";
     }
  
     @Override
