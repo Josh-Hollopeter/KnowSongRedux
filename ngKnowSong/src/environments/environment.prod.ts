62d4723d8a6d6f5f33fb39ce0e9856831dbfbacb
NealSavant <nealrsavant@gmail.com>
2020-05-19 23:09:17 -0600

    Front end setup for artist search

--- ngKnowSong/src/environments/environment.prod.ts
+++ ngKnowSong/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // temp so we can test in production environment
-  baseUrl: 'http://localhost:8085/'
+  baseUrl: 'http://knowsong.life:8080/KnowSongRest/'
 };
