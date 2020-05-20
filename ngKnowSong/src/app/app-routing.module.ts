a225c991970393d6aa3afe3f01271bd8803f48e0
NealSavant <nealrsavant@gmail.com>
2020-05-19 23:09:17 -0600

    Front end setup for artist search

--- ngKnowSong/src/app/app-routing.module.ts
+++ ngKnowSong/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './component/landing/landing.component';
 import { HomeComponent } from './component/home/home.component';
+import { CreateGameComponent } from './component/create-game/create-game.component';
 
 
 const routes: Routes = [
   
   { path: 'home', component: HomeComponent},
+  { path: 'creategame', component: CreateGameComponent},
   { path: '**', component: LandingComponent}
 ];
 
