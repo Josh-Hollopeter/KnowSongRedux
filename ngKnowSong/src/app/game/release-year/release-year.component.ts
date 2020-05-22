451ca969faabbb347e5892ab5c21da534d30e439
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-05-22 10:15:12 -0400

    added new angular components for game layout

---
+++ ngKnowSong/src/app/game/release-year/release-year.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-release-year',
+  templateUrl: './release-year.component.html',
+  styleUrls: ['./release-year.component.css']
+})
+export class ReleaseYearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
