1ac24a544f1d07fa1184538100192e2ff84e5468
NealSavant <nealrsavant@gmail.com>
2020-05-24 02:20:24 -0600

    I have mastered the art of the observable. login page redirects to user home if you are logged in. landing page is default '' route. any bad urls redirect to landing page (then to home if logged in)

--- ngKnowSong/src/app/app-routing.module.ts
+++ ngKnowSong/src/app/app-routing.module.ts
@@ -5,22 +5,20 @@ import { LandingComponent } from './component/landing/landing.component';
 import { HomeComponent } from './component/home/home.component';
 import { CreateGameComponent } from './component/create-game/create-game.component';
 import { AdminGuard } from './admin/admin.guard';
-import { CallbackComponent } from './component/callback/callback.component';
 import { UserResolverService } from './service/resolver/user-resolver.service';
 import { ReleaseYearComponent } from './game/release-year/release-year.component';
 import { NameSongComponent } from './game/name-song/name-song.component';
 
 
 const routes: Routes = [
-  // { path: '**', redirectTo: '', pathMatch: 'full' },
+  
   { path: '', component: LandingComponent },
   { path: 'home', component: HomeComponent, resolve: {user: UserResolverService}, canActivate: [AdminGuard]},
   { path: 'creategame', component: CreateGameComponent, canActivate: [AdminGuard]},
-  { path: 'loggingIn', component: CallbackComponent, canActivate: [AdminGuard]},
   { path: 'LyricMatch', component: LyricMatchComponent, canActivate: [AdminGuard]},
   { path: 'ReleaseYear' , component: ReleaseYearComponent, canActivate:[AdminGuard]},
-  { path: 'NameSong', component: NameSongComponent, canActivate:[AdminGuard]}
-
+  { path: 'NameSong', component: NameSongComponent, canActivate:[AdminGuard]},
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({
