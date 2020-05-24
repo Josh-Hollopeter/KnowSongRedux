a588e156793d2e054e87b32d755502dccdee557e
NealSavant <nealrsavant@gmail.com>
2020-05-24 02:20:24 -0600

    I have mastered the art of the observable. login page redirects to user home if you are logged in. landing page is default '' route. any bad urls redirect to landing page (then to home if logged in)

--- ngKnowSong/src/app/component/landing/landing.component.ts
+++ ngKnowSong/src/app/component/landing/landing.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { User } from 'src/app/model/user.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 // landing page where users can login through spotify
 @Component({
@@ -24,6 +26,19 @@ export class LandingComponent implements OnInit {
   ngOnInit(): void {
     ViewEncapsulation.ShadowDom;
 
+    if(this.checkLogin())
+      this.router.navigate(['home']);
+    
+  }
+
+  checkLogin(): Observable<boolean>{
+    return this.authService.isLoggedIn().pipe(map( (authorized: boolean) => {
+      if(authorized)
+        return true;
+      
+      return false;
+      })
+    ); 
   }
 
   // this function authenticates the client with the server and registers the user into database
