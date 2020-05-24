1ea0b5d51c0226d097293e598d25a6b1c6a07b14
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-05-24 19:06:17 -0400

    change in global error handler

--- ngKnowSong/src/app/global-error-handler.service.ts
+++ ngKnowSong/src/app/global-error-handler.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, ErrorHandler, Injector } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http'
 @Injectable({
-  providedIn: 'root'
+providedIn: 'root'
 })
-export class GlobalErrorHandlerService {
-
-  constructor() { }
+export class ErrorService implements ErrorHandler{
+ constructor(private injector: Injector) { }
+ handleError(error: any) {
+  const router = this.injector.get(Router);
+  if (Error instanceof HttpErrorResponse) {
+   console.log(error.status);
+  }
+  else {
+   console.error("an error occurred here broo");
+  }
+  router.navigate(['error']);
+  }
 }
