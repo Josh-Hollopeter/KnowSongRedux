f0f62d1aad4b153feded8c8ae4469aa88ba449a3
NealSavant <nealrsavant@gmail.com>
2020-05-24 04:21:31 -0600

    Greatly simplified the API calls to spotify. never write the same code twice right? same applies here. its lit.

--- ngKnowSong/src/app/service/spotify-api.service.ts
+++ ngKnowSong/src/app/service/spotify-api.service.ts
@@ -37,129 +37,65 @@ export class SpotifyAPIService {
     //   this.accessToken = <string>response;
     //   this.httpOptions.headers.set('Authorization', `Bearer ${this.accessToken}`);
     // }));
-    //keeping this here because, for some reason. i couldnt figure out why this pipe would not work, yet a subscribe DID... hnngg
+    //keeping this here because, for some reason. i couldnt figure out why this pipe would not even be called, yet a subscribe DID... hnngg
   }
 
+  hitSpotify(url: string) {
+
+    return this.http.get(url, this.httpOptions).pipe(
+      map((event: HttpResponse<any>)=> {
+        return event; 
+      }),
+      catchError(err => {
+        if(err.status == 401){
+          this.refreshAccessToken();
+        }
+        return null;
+      })
+    );
+
+  }
   //---------------------
   //-  Playlist Methods -
   //---------------------
   getUserPlaylists() {
     let url = "https://api.spotify.com/v1/me/playlists?limit=50&offset=0";
 
-    return this.http.get(url, this.httpOptions).pipe(
-      map((event: HttpResponse<any>)=> {
-        if(event.status == 401){
-          this.refreshAccessToken();
-        }
-        return event; 
-      }),
-      catchError(err => {
-        if(err.status == 401){
-          this.refreshAccessToken();
-        }
-        console.log(err);
-        
-        return [];
-      })
-    );
-
+    return this.hitSpotify(url);
   }
 
   getTracksFromPlaylist(playlistId: string) {
     let url = "https://api.spotify.com/v1/playlists/"+ playlistId +"/tracks";
 
-    return this.http.get(url, this.httpOptions).pipe(
-      map((event: HttpResponse<any>)=> {
-        return event; 
-      }),
-      catchError(err => {
-        if(err.status == 401){
-          this.refreshAccessToken();
-        }
-        console.log(err);
-        
-        return [];
-      })
-    );
+    return this.hitSpotify(url);
   }
   //---------------------
   //-   Artist Methods  -
   //---------------------
 
   searchArtist(artistName: string) {
-
     let url = "https://api.spotify.com/v1/search?q=" + artistName + "&type=artist&limit=5";
     
-    return this.http.get(url, this.httpOptions).pipe(
-      map((event: HttpResponse<any>)=> {
-        return event; 
-      }),
-      catchError(err => {
-        if(err.status == 401){
-          this.refreshAccessToken();
-        }
-        console.log(err);
-        
-        return [];
-      })
-    );
+    return this.hitSpotify(url);
   }
 
   getAlbumsFromArtist(artistId: string) {
     let url = "https://api.spotify.com/v1/artists/" + artistId + "/albums";
 
-    return this.http.get(url, this.httpOptions).pipe(
-      map((event: HttpResponse<any>)=> {
-        return event; 
-      }),
-      catchError(err => {
-        if(err.status == 401){
-          this.refreshAccessToken();
-        }
-        console.log(err);
-        
-        return [];
-      })
-    );
+    return this.hitSpotify(url);
   }
 
   getTracksFromAlbum(albumId: string) {
     let url = "https://api.spotify.com/v1/albums/" + albumId + "/tracks?limit=50";
 
-    return this.http.get(url, this.httpOptions).pipe(
-      map((event: HttpResponse<any>)=> {
-        if(event.status == 401){
-          this.refreshAccessToken();
-        }
-        return event; 
-      }),
-      catchError(err => {
-        if(err.status == 401){
-          this.refreshAccessToken();
-        }
-        console.log(err);
-        
-        return [];
-      })
-    );
+    return this.hitSpotify(url);
   }
 
 
   getAudioFeaturesTracks(csv: string){
     let url = "https://api.spotify.com/v1/audio-features/?ids=" + csv;
 
-    return this.http.get(url, this.httpOptions).pipe(
-      map((event: HttpResponse<any>)=> {
-        return event; 
-      }),
-      catchError(err => {
-        if(err.status == 401){
-          this.refreshAccessToken();
-        }
-        console.log(err);
-        
-        return [];
-      })
-    );
+    return this.hitSpotify(url);
   }
+  
 }
