5bef7bd6cd0271f2bd5306fae43a53c53fe590b0
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-05-27 11:16:11 -0400

    13 out of 27 basic tests passed

--- ngKnowSong/src/app/app.component.spec.ts
+++ ngKnowSong/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -6,7 +7,9 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule,
+        HttpXhrBackend,
       ],
       declarations: [
         AppComponent
