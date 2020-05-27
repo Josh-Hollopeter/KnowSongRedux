aa1b5aa9f29ce93d5cab0a7c4261ce049cb00c1b
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-05-27 10:31:33 -0400

    base setup for testing tests failing

--- ngKnowSong/e2e/src/app.po.ts
+++ ngKnowSong/e2e/src/app.po.ts
@@ -1,8 +1,8 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo(){
+    return browser.get(browser.baseUrl);
   }
 
   getTitleText(): Promise<string> {
