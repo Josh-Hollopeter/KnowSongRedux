fcc72e0c254595225c07a5cfdeb29a2f390c0b95
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-05-28 13:15:09 -0400

    17 basic tests passed

--- ngKnowSong/src/app/component/landing/landing.component.spec.ts
+++ ngKnowSong/src/app/component/landing/landing.component.spec.ts
@@ -1,6 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
+import { MusixMatchService } from './../../service/API/musix-match.service';
+import { SpotifyAPIService } from './../../service/API/spotify-api.service';
+import { UserService } from './../../service/Data/user.service';
+import { CreateGameComponent } from './../../component/create-game/create-game.component';
+import { MusicDataService } from './../../game/data/music-data.service';
+import { KnowSongComponent } from './../../game/know-song/know-song.component';
+import { ReleaseYearComponent } from './../../game/release-year/release-year.component';
+import { LyricMatcherComponent } from './../../game/lyric-matcher/lyric-matcher.component';
+import { HomeComponent } from './../../component/home/home.component';
+import { AppComponent } from './../../app.component';
+import { AppRoutingModule } from './../../app-routing.module';
+import { ErrorComponent } from './../../error/error.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
+import { AuthService } from 'src/app/service/API/auth.service'
+import { FormsModule } from '@angular/forms';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,7 +32,8 @@ describe('LandingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
+      declarations: [ LandingComponent ],
+      imports:[HttpClientModule, AppRoutingModule]
     })
     .compileComponents();
   }));
