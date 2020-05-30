dd8b5da28a518079a803779dff0f9a0d3099460a
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-05-30 14:08:25 -0400

    removed unused imports from test classes

--- ngKnowSong/src/app/game/lyric-matcher/lyric-matcher.component.spec.ts
+++ ngKnowSong/src/app/game/lyric-matcher/lyric-matcher.component.spec.ts
@@ -1,30 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LyricMatcherComponent } from './lyric-matcher.component';
-import { SpotifyAPIService } from 'src/app/service/API/spotify-api.service';
-import { MusixMatchService } from 'src/app/service/API/musix-match.service';
 
-import { KnowSongComponent } from './../../game/know-song/know-song.component';
-import { MusicDataService } from './../../game/data/music-data.service';
-import { ReleaseYearComponent } from './../../game/release-year/release-year.component';
-import { CreateGameComponent } from './../../component/create-game/create-game.component';
-import { LandingComponent } from './../../component/landing/landing.component';
-import { HomeComponent } from './../../component/home/home.component';
-import { AppComponent } from './../../app.component';
 import { AppRoutingModule } from './../../app-routing.module';
-import { ErrorComponent } from './../../error/error.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
-import { AuthService } from 'src/app/service/API/auth.service'
-import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
 
 describe('LyricMatcherComponent', () => {
   let component: LyricMatcherComponent;
