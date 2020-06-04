ec945ca3f40d1f22206ec25b52e8959fe8d65db0
NealSavant <nealrsavant@gmail.com>
2020-06-04 17:56:33 -0600

    Entities, server side requests to spotify auth server, controllers updated

--- KnowSongJPA/src/main/java/life/knowsong/entities/Genre.java
+++ KnowSongJPA/src/main/java/life/knowsong/entities/Genre.java
@@ -1,27 +1,34 @@
 package life.knowsong.entities;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "genre")
 public class Genre {
 
 	@Id
 	private String name;
 	
 	@JsonIgnore
-	@ManyToMany
+	@ElementCollection
+	@ManyToMany(mappedBy = "artist",fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
 	@JoinTable(name="artist_genre",
 			joinColumns=@JoinColumn(name="genre_name"),
 			inverseJoinColumns=@JoinColumn(name="artist_id"))
-	private Set<Artist> Artists;
+	private Set<Artist> Artists = new HashSet<Artist>();
 
 	public String getName() {
 		return name;
@@ -34,10 +41,5 @@ public class Genre {
 	public Set<Artist> getArtists() {
 		return Artists;
 	}
-
-	public void setArtists(Set<Artist> artists) {
-		Artists = artists;
-	}
-	
-	
+		
 }
--- KnowSongRest/src/main/java/life/knowsong/http/SpotifyRequestImpl.java
+++ KnowSongRest/src/main/java/life/knowsong/http/SpotifyRequestImpl.java
@@ -2,4 +2,9 @@ package life.knowsong.http;
 
 public class SpotifyRequestImpl implements SpotifyRequest {
 
+	
+	public void getArtistCollection(String authToken, String artistId) {
+		
+	}
+	
 }
