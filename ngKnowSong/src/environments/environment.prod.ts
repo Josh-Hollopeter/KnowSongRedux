5bf1579ca5d0e36755362fc8825d20f15ddc983e
NealSavant <nealrsavant@gmail.com>
2020-06-11 18:40:41 -0600

    changes

--- ngKnowSong/src/environments/environment.prod.ts
+++ ngKnowSong/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // temp so we can test in production environment
-  baseUrl: 'http://knowsong.life:8080/KnowSongRest/'
+  baseUrl: '/KnowSongRest/'
 };
