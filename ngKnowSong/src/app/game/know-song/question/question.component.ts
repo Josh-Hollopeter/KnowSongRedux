7b0a0df9c48c2196884b2b912eb9d7d9a5a76388
NealSavant <nealrsavant@gmail.com>
2020-06-18 05:14:04 -0600

    added an animation that isnt working yet. prevents user from going to next question before playing track

--- ngKnowSong/src/app/game/know-song/question/question.component.ts
+++ ngKnowSong/src/app/game/know-song/question/question.component.ts
@@ -1,37 +1,57 @@
 import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy, OnChanges, SimpleChanges, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { SingleplayerQuestion } from 'src/app/model/singleplayer-question.model';
 import { KnowSongComponent } from '../know-song.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
+  animations: [
+    trigger('clickMe', [
+      state('normal', style({
+        backgroundColor: 'transparent'
+      })),
+      state('listen', style({
+        backgroundColor: 'yellow'
+      })),
+      transition('normal <=> listen', [
+        animate('0.2s')
+      ]),
+    ]),
+  ]
 })
-export class QuestionComponent implements AfterViewInit {
+export class QuestionComponent implements AfterViewInit, OnChanges {
 
   @Input() questionOptions: Array<string>;
   @Input() newAudio: string;
 
   @Output() optionPicked: EventEmitter<string> = new EventEmitter<string>();
 
-  @ViewChild('audioElement', {static: false}) 
-  public _audioRef: ElementRef;
+  @ViewChild('audioElement', {static: false}) public _audioRef: ElementRef;
+
   private audio: HTMLAudioElement;
 
-
+  public listened:boolean;
   constructor() { }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.listened = false;
+  }
 
   public ngAfterViewInit() {
     this.audio = this._audioRef.nativeElement;
     if (this.audio) {
       this.audio.src = this.newAudio;
-      
     }
   }
 
 
   setUserResponse(answer: string){
-    this.optionPicked.emit(answer);
+    if(this.listened){
+      this.optionPicked.emit(answer);
+    }
   }
 
   public restart(): void{
@@ -41,7 +61,7 @@ export class QuestionComponent implements AfterViewInit {
   public play(){
     this.audio.load();
     this.audio.play();
+    this.listened = true;
   }
-
 
 }
