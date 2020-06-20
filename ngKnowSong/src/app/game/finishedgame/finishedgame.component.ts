400ceb05227522ab9962889daebd69ea01fe5cdb
NealSavant <nealrsavant@gmail.com>
2020-06-19 22:54:41 -0600

    Removed album year trivia. Setup page redirect when loading or refreshing /audio or /lyric without an artist selected

---
+++ ngKnowSong/src/app/game/finishedgame/finishedgame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-finishedgame',
+  templateUrl: './finishedgame.component.html',
+  styleUrls: ['./finishedgame.component.css']
+})
+export class FinishedgameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- ngKnowSong/src/app/game/resolver/lyric-resolver.service.ts
+++ ngKnowSong/src/app/game/resolver/lyric-resolver.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
+import { Resolve, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SingleplayerGame } from 'src/app/model/singleplayer-game.model';
+import { MusicDataService } from '../data/music-data.service';
+import { GameBuilderService } from 'src/app/service/API/game-builder.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LyricResolverService implements Resolve<any>{
 
-  constructor() { }
-  resolve(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot) {
-    throw null;
+  constructor(
+    private musicDataService: MusicDataService,
+    private gameBuilder: GameBuilderService,
+    private router: Router
+  ) { }
+  
+  resolve(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot):
+  Observable<SingleplayerGame> | Promise<SingleplayerGame> | SingleplayerGame {
+    
+    if(this.musicDataService.getArtist() === undefined){
+      this.router.navigate(['home']);
+    }
+    
+    return this.gameBuilder.buildAudioGame(this.musicDataService.getArtist().id);
+    
   }
 }
