599e60face53b4ebeda9304c6c23d9642b17d7ee
NealSavant <nealrsavant@gmail.com>
2020-06-24 23:13:38 -0600

    Working on game history

--- ngKnowSong/src/app/model/singleplayer-game.model.ts
+++ ngKnowSong/src/app/model/singleplayer-game.model.ts
@@ -5,16 +5,19 @@ export class SingleplayerGame {
     description: string;
     questions: SingleplayerQuestion[];
     played: Date;
+    id: number;
 
 
     constructor(
         description?: string,
         questions?: SingleplayerQuestion[],
         played?: Date,
-        
+        id?: number
     ){
 
         this.description = description;
         this.questions = questions;
+        this.played = played;
+        this.id = id;
     }
 }
