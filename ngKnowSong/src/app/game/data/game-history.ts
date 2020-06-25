76a954091e20393567dd4e5ec9b446a3878e9906
NealSavant <nealrsavant@gmail.com>
2020-06-25 01:47:35 -0600

    upgraded to angular 10

--- ngKnowSong/src/app/game/data/game-history.ts
+++ ngKnowSong/src/app/game/data/game-history.ts
@@ -1,5 +1,7 @@
 import { SingleplayerGame } from 'src/app/model/singleplayer-game.model';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class GameHistory {
 
     public singleplayerGameHistory: Array<SingleplayerGame>
