7b82c2ab97af207dfe7cb8c02bf369405f6564f5
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-06-28 19:27:12 -0400

    working deployment ant matchers not setup correctly

--- KnowSongRest/src/main/java/life/knowsong/security/SecurityConfig.java
+++ KnowSongRest/src/main/java/life/knowsong/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests(a -> a
-//                .antMatchers("/").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()//Specify that URLs are allowed by any authenticated user.
                 
             )
